f90336e89287b0979476f20b76cedb808a336d51
ROKI MONDAL <rokimondal@ROKIs-MacBook-Air.local>
2025-09-25 18:42:35 +0530

    update

--- backend/src/lib/email.js
+++ backend/src/lib/email.js
@@ -1,30 +1,39 @@
-import sgMail from '@sendgrid/mail';
+import nodemailer from "nodemailer"
 import { VerificationEmail, WelcomeEmail } from "../constants/emailTemplate.js";
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
 
 export const sendVerificationEmail = async (email, code) => {
     try {
-        await sgMail.send({
+        const response = await transporter.sendMail({
+            from: `"Netnity" ${process.env.EMAIL_USER}`,
             to: email,
-            from: 'your-email@example.com', // Verified sender
             subject: "🔐 Verify Your Email",
             html: VerificationEmail(code),
         });
     } catch (error) {
         console.log('Email error : ', error);
     }
-};
+}
 
-export const sendWelcomeEmail = async (email, name) => {
+export const sendWellcomeEmail = async (email, name) => {
     try {
-        await sgMail.send({
+        const response = await transporter.sendMail({
+            from: `"Netnity" ${process.env.EMAIL_USER}`,
             to: email,
-            from: 'your-email@example.com', // Verified sender
             subject: "🎉 Welcome to Netnity!",
             html: WelcomeEmail(name),
         });
+
     } catch (error) {
         console.log('Email error : ', error);
     }
-};
+}
