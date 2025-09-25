4bafc4e1119896c2a18619c0fe25396c3a0a7b3a
ROKI MONDAL <rokimondal@ROKIs-MacBook-Air.local>
2025-09-25 17:01:00 +0530

    update nodemailer

--- backend/src/lib/email.js
+++ backend/src/lib/email.js
@@ -3,8 +3,8 @@ import { VerificationEmail, WelcomeEmail } from "../constants/emailTemplate.js";
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
